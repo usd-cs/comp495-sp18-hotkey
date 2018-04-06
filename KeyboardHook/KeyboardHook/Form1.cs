c929267537528f4b40aa692607e073fa6f173af8
Sean Mulvey <myblunda@yahoo.com>
2018-04-06 08:38:53 -0700

    Super basic but if you run this and type A it will output Q instead with no errors. Becomes implementing this with your guys code now

---
+++ KeyboardHook/KeyboardHook/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyboardHook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            KeyboardHook newHook = new KeyboardHook((int)KeyboardHook.Modifiers.None, Keys.A, this);
+
+            hook.Register(); // registering globally that A will call a method
+        }
+
+        protected override void WndProc(ref Message cmd)
+        {
+            if (cmd.Msg == 0x0312)
+                HandleHotkey(); // A, which was registered before, was pressed
+            base.WndProc(ref m);
+        }
+
+        private void HandleHotkey()
+        {
+            // instead of A send Q
+            KeyboardManager.PressKey(Keys.Q);
+        }
+    }
+}
---
+++ KeyboardHook/KeyboardHook/KeyboardManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyboardHook
+{
+    public class KeyboardManager
+    {
+        public const int INPUT_KEYBOARD = 1;
+        public const int KEYEVENTF_KEYUP = 0x0002;
+
+        public struct KEYDBINPUT
+        {
+            public Int16 wVk;
+            public Int16 wScan;
+            public Int32 dwFlags;
+            public Int32 time;
+            public Int32 dwExtraInfo;
+            public Int32 __filler1;
+            public Int32 __filler2;
+        }
+
+        public struct INPUT
+        {
+            public Int32 type;
+            public KEYDBINPUT ki;
+        }
+
+        [DllImport("user32")]
+        public static extern int SendInput(int cInputs, ref INPUT pInputs, int cbSize);
+
+        public static void HoldKey(Keys vk)
+        {
+            INPUT input = new INPUT();
+            input.type = INPUT_KEYBOARD;
+            input.ki.dwFlags = 0;
+            input.ki.wVk = (Int16)vk;
+            SendInput(1, ref input, Marshal.SizeOf(input));
+        }
+
+        public static void ReleaseKey(Keys vk)
+        {
+            INPUT input = new INPUT();
+            input.type = INPUT_KEYBOARD;
+            input.ki.dwFlags = KEYEVENTF_KEYUP;
+            input.ki.wVk = (Int16)vk;
+            SendInput(1, ref input, Marshal.SizeOf(input));
+        }
+
+        public static void PressKey(Keys vk)
+        {
+            HoldKey(vk);
+            ReleaseKey(vk);
+        }
+    }
+}
