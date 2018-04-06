3e9bc72a9f7f39a661e8c2f2d2c69046d3b029e3
Sean Mulvey <seanpatrickmulvey@gmail.com>
2018-04-06 14:12:18 -0700

    Updated with comments.

--- KeyboardHook/KeyboardHook/KeyboardHook.cs
+++ KeyboardHook/KeyboardHook/KeyboardHook.cs
@@ -35,11 +35,14 @@ namespace KeyboardHook
             this.modifier = modifiers;
             this.key = (int)key;
             this.hWnd = f.Handle;
-            id = this.GetHashCode();
+            id = this.GetHashCode(); //this is used to identify what hotkey has been pressed see function
         }
 
         public override int GetHashCode()
         {
+            //Since each Hotkey is created differently, each hotkey will have a different hashcode associated with it
+            //To make sure of this, use the keys themselves to make the code. We will therefore not repeat because
+            //We ony have one instance of whatever that hotkey is
             return modifier ^ key ^ hWnd.ToInt32();
         }
 
--- KeyboardHook/KeyboardHook/KeyboardManager.cs
+++ KeyboardHook/KeyboardHook/KeyboardManager.cs
@@ -30,11 +30,20 @@ namespace KeyboardHook
             public KEYDBINPUT ki;
         }
 
+        //This is what overrides the OS and sends out input instead
         [DllImport("user32")]
         public static extern int SendInput(int cInputs, ref INPUT pInputs, int cbSize);
-
+        //This is used for if someone holds down the 'a' button it will repeatedly run until the press up
         public static void HoldKey(Keys vk)
         {
+            //This takes some reading
+            //https://msdn.microsoft.com/en-us/library/windows/desktop/ms646270(v=vs.85).aspx
+            //Basically the glag setup is saying
+            //Get from the keyboard input
+            //ki = Keyboard input
+            //dwflag = what the key is doing 0 is the default which we need to refresh each time so it = 0
+            //wVk = the keyboard input value (something from 1-254)
+            //Then we send all this info along
             INPUT input = new INPUT();
             input.type = INPUT_KEYBOARD;
             input.ki.dwFlags = 0;
@@ -44,6 +53,8 @@ namespace KeyboardHook
 
         public static void ReleaseKey(Keys vk)
         {
+            //Similar to the above function, big difference in the dwFlag is for keyboard up which means
+            //The input is over send last signal
             INPUT input = new INPUT();
             input.type = INPUT_KEYBOARD;
             input.ki.dwFlags = KEYEVENTF_KEYUP;
@@ -53,6 +64,7 @@ namespace KeyboardHook
 
         public static void PressKey(Keys vk)
         {
+            //This will stay in holdkey until it is over
             HoldKey(vk);
             ReleaseKey(vk);
         }
